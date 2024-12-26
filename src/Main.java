299276d4684c59bb2d4f9ba1af93a818ca948c97
desti <destinjdonahue@gmail.com>
2024-12-26 14:53:56 -0500

    Done.

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,5 @@ import static KernelLand.OS.*;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         startup(new IdleProcess());
-
-        OS.createProcess(new World("Fuck me you little cum slut"));
-        OS.createProcess(new World("Massive fart"));
-        OS.createProcess(new Plead());
-
-
     }
 }
